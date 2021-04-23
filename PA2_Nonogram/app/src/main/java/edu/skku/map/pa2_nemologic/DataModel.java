0b84b75983a85b734d9f4c7ef86944dd93ae3101
hanwoolsky <hhw0925@naver.com>
2021-04-23 16:15:30 +0900

    [add]PA2_mid.

---
+++ PA2_Nonogram/app/src/main/java/edu/skku/map/pa2_nemologic/DataModel.java
@@ -0,0 +1,14 @@
+package edu.skku.map.pa2_nemologic;
+
+public class DataModel {
+    String link;
+    String thumbnail;
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+}
